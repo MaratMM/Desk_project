b901b34306cb6d986b64e2b7ed0074d92e12fd1c
MaratMM <woodi94@mail.ru>
2022-02-11 20:47:01 +0300

    сверстали каталог и индекс осталось разобраться с свг иконкой, не заливается цвет и нужно добавить плавную анимацию открытия и закрытия меню

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,25 +1,27 @@
 //необходимая часть , не удалять!!!!!!!!!!
-//alert('Hello gulp!');;
+////alert('Hello gulp!');;
 
 function testWebP(callback) {
 
     var webP = new Image();
     webP.onload = webP.onerror = function () {
-    callback(webP.height == 2);
+        callback(webP.height == 2);
     };
     webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
-    }
-    
-    testWebP(function (support) {
-    
+}
+
+testWebP(function (support) {
+
     if (support == true) {
-    document.querySelector('body').classList.add('webp');
-    }else{
-    document.querySelector('body').classList.add('no-webp');
+        document.querySelector('body').classList.add('webp');
+    } else {
+        document.querySelector('body').classList.add('no-webp');
     }
-    });
+});
 
 //основной скрипт
+
+//import "./btnCloseMenu";
 
 //скрипт выпадающего меню по нажатию на кнопки
 let btnCloseMenu=document.querySelector(".btn_close_menu");
@@ -45,3 +47,19 @@ iconBurger.addEventListener('click', function(evt) {
     document.querySelector('.div_icons').classList.add('vip_menu');
     document.querySelector('body').classList.add('lock_page');
 });;
+
+//////////////нижняя часть задумка, боловство учимся прогать
+
+window.addEventListener('DOMContentLoaded', function () {
+    const salePrice = document.querySelectorAll('li.catalog__section__list__item.sale>span.catalog__section__list__item__price>span.catalog__section__list__item__price__sale');
+
+    console.log(salePrice);
+
+    for (let i = 0; i < salePrice.length; i++) {
+        console.log(salePrice[i].getBoundingClientRect().width);
+    }
+
+
+});
+
+
